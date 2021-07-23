4edd0eb7c7c712fb5cdbd08960bbfe13709cdcb0
msjin9208 <msjin9208@gmail.com>
2021-07-23 18:49:30 +0900

    mapSetting

--- msjin/Assets/Script/SceneUI/InventorySceneUI.cs
+++ msjin/Assets/Script/SceneUI/InventorySceneUI.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InventorySceneUI : MonoBehaviour
+public class InventorySceneUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button _backBtn;
+    private void Start()
     {
-        
+        _backBtn.onClick.AddListener(ScenePreviewEnter);
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void SceneEnter(SceneBase scene)
     {
-        
+        base.SceneEnter(scene);
+    }
+    public override void ScenePreviewEnter()
+    {
+        base.ScenePreviewEnter();
     }
 }
